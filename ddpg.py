d9cffad0d8f87d2f0359a470caf62ce68942187c
Mode <mode89@mail.ru>
2018-06-18 19:03:40 +0800

    Specify types of command line arguments

--- ddpg.py
+++ ddpg.py
@@ -349,19 +349,49 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='provide arguments for DDPG agent')
 
     # agent parameters
-    parser.add_argument('--actor-lr', help='actor network learning rate', default=0.0001)
-    parser.add_argument('--critic-lr', help='critic network learning rate', default=0.001)
-    parser.add_argument('--gamma', help='discount factor for critic updates', default=0.99)
-    parser.add_argument('--tau', help='soft target update parameter', default=0.001)
-    parser.add_argument('--buffer-size', help='max size of the replay buffer', default=1000000)
-    parser.add_argument('--minibatch-size', help='size of minibatch for minibatch-SGD', default=64)
+    parser.add_argument('--actor-lr',
+        help='actor network learning rate',
+        type=float,
+        default=0.0001)
+    parser.add_argument('--critic-lr',
+        help='critic network learning rate',
+        type=float,
+        default=0.001)
+    parser.add_argument('--gamma',
+        help='discount factor for critic updates',
+        type=float,
+        default=0.99)
+    parser.add_argument('--tau', help='soft target update parameter',
+        type=float,
+        default=0.001)
+    parser.add_argument('--buffer-size',
+        help='max size of the replay buffer',
+        type=int,
+        default=1000000)
+    parser.add_argument('--minibatch-size',
+        help='size of minibatch for minibatch-SGD',
+        type=int,
+        default=64)
 
     # run parameters
-    parser.add_argument('--env', help='choose the gym env- tested on {Pendulum-v0}', default='Pendulum-v0')
-    parser.add_argument('--random-seed', help='random seed for repeatability', default=1234)
-    parser.add_argument('--max-episodes', help='max num of episodes to do while training', default=50000)
-    parser.add_argument('--max-episode-len', help='max length of 1 episode', default=1000)
-    parser.add_argument('--render-env', help='render the gym env', action='store_true')
+    parser.add_argument('--env',
+        help='choose the gym env- tested on {Pendulum-v0}',
+        default='Pendulum-v0')
+    parser.add_argument('--random-seed',
+        help='random seed for repeatability',
+        type=int,
+        default=1234)
+    parser.add_argument('--max-episodes',
+        help='max num of episodes to do while training',
+        type=int,
+        default=50000)
+    parser.add_argument('--max-episode-len',
+        help='max length of 1 episode',
+        type=int,
+        default=1000)
+    parser.add_argument('--render-env',
+        help='render the gym env',
+        action='store_true')
     
     args = vars(parser.parse_args())
     
