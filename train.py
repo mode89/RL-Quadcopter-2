bff3f1609d4317a8da1628ebe710c30244e7d913
Mode <mode89@mail.ru>
2018-07-08 12:35:48 +0800

    Create summary file at the end of training

--- train.py
+++ train.py
@@ -9,32 +9,34 @@ def train(agent, task):
     rewards = deque(maxlen=100)
     summary = list()
 
-    for episodeId in range(100000):
-        episodeReward = 0.0
-        state = task.reset()
+    try:
+        for episodeId in range(100000):
+            episodeReward = 0.0
+            state = task.reset()
 
-        stepCount = 0
-        while True:
-            stepCount += 1
-            action = agent.act(state)
-            nextState, reward, done = task.step(action[0])
-            agent.learn(nextState=nextState, reward=reward, done=done)
-            state = nextState
-            episodeReward += reward
-            if done: break
+            stepCount = 0
+            while True:
+                stepCount += 1
+                action = agent.act(state)
+                nextState, reward, done = task.step(action[0])
+                agent.learn(nextState=nextState, reward=reward, done=done)
+                state = nextState
+                episodeReward += reward
+                if done: break
 
-        rewards.append(episodeReward)
-        avgReward = np.mean(rewards)
+            rewards.append(episodeReward)
+            avgReward = np.mean(rewards)
 
-        print("Episode: {} Reward: {:4.3f} Avg. reward: {:4.3f}". \
-            format(episodeId, episodeReward, avgReward))
+            print("Episode: {} Reward: {:4.3f} Avg. reward: {:4.3f}". \
+                format(episodeId, episodeReward, avgReward))
 
-        summary.append({
-            "episodeId": episodeId,
-            "reward": episodeReward,
-            "averageReward": avgReward,
-        })
-
+            summary.append({
+                "episodeId": episodeId,
+                "reward": episodeReward,
+                "averageReward": avgReward,
+            })
+    finally:
+        print("Dumping summary ...")
         with open("summary.json", "w") as summaryFile:
             json.dump(summary, summaryFile, indent=True)
 
